14d0bcdfc557759f71e8367170588170f946c390
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2025-09-17 20:24:42 -0300

    refactor: change to OPTIMISTIC_LOCK

--- src/main/java/br/com/mercadolibre/infra/sql/stock/repository/StockRepository.java
+++ src/main/java/br/com/mercadolibre/infra/sql/stock/repository/StockRepository.java
@@ -1,7 +1,6 @@
 package br.com.mercadolibre.infra.sql.stock.repository;
 
 import br.com.mercadolibre.infra.sql.stock.model.StockEntity;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -11,10 +10,12 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import static jakarta.persistence.LockModeType.OPTIMISTIC;
+
 @Repository
 public interface StockRepository extends JpaRepository<StockEntity, UUID> {
 
-    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Lock(OPTIMISTIC)
     @Query("SELECT s FROM StockEntity s WHERE s.product.id = :productId AND s.store.id = :storeId")
     Optional<StockEntity> findByProductIdAndStoreIdWithLock(@Param("productId") UUID productId, @Param("storeId") UUID storeId);
 }
